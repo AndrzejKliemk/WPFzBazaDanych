00ca3b2b91b3be10c0400cd45294c60d780785f7
AndrzejKliemk <andrzejklimek96@gmail.com>
2021-06-30 15:06:54 +0200

    Basic

---
+++ WPFzBazaDanych/LoginScreen.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFzBazaDanych
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=localhost\SQLEXPRESS; Initial Catalog=LoginDataBase; Integrated Security=True;");
+            try
+            {
+                if (sqlCon.State == System.Data.ConnectionState.Closed)
+                    sqlCon.Open();
+                String query = "SELECT COUNT(1) FROM DBUsers WHERE Username=@Username AND Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = System.Data.CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", UsernameText.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", PasswordText.Text);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    MainWindow window = new MainWindow();
+                    window.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or password is wrong");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            Register window = new Register();
+            window.Show();
+            this.Close();
+        }
+    }
+}
---
+++ WPFzBazaDanych/Register.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFzBazaDanych
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            if (PasswordText.Text == PasswordTextConfirm.Text)
+            {
+
+
+                SqlConnection sqlCon = new SqlConnection(@"Data Source=localhost\SQLEXPRESS; Initial Catalog=LoginDataBase; Integrated Security=True;");
+                try
+                {
+                    if (sqlCon.State == System.Data.ConnectionState.Closed)
+                        sqlCon.Open();
+                    String query = "SELECT COUNT(1) FROM DBUsers WHERE Username=@Username";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.CommandType = System.Data.CommandType.Text;
+                    sqlCmd.Parameters.AddWithValue("@Username", UsernameText.Text);
+                    int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                    if (count == 1)
+                    {
+                        MessageBox.Show("Such user exist!");
+                        sqlCon.Close();
+                    }
+                    else
+                    {
+                        query = "INSERT INTO DBUsers (Username, Password) VALUES (@Username, @Password)";
+                        sqlCmd = new SqlCommand(query, sqlCon);
+                        sqlCmd.CommandType = System.Data.CommandType.Text;
+                        sqlCmd.Parameters.AddWithValue("@Username", UsernameText.Text);
+                        sqlCmd.Parameters.AddWithValue("@Password", PasswordText.Text);
+
+
+                        int result = sqlCmd.ExecuteNonQuery();
+
+                        // Check Error
+                        if (result < 0)
+                            MessageBox.Show("Error inserting data into Database!");
+
+                        MainWindow window = new MainWindow();
+                        window.Show();
+                        this.Close();
+                    }
+
+
+
+
+
+
+
+
+
+                    
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sqlCon.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Both passwords need to be same.");
+            }
+        }
+    }
+}
